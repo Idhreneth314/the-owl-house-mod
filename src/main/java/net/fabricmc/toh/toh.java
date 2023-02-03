45fd6f918ab47d0f91c3e0ade79300ee4fd9a9e7
Idhreneth314 <idhreneth314@protonmail.com>
2023-02-03 16:36:29 -0500

    Added Feral Biped Beasts

--- src/main/java/net/fabricmc/toh/toh.java
+++ src/main/java/net/fabricmc/toh/toh.java
@@ -5,8 +5,6 @@ import net.fabricmc.toh.block.ModBlocks;
 import net.fabricmc.toh.item.ModItems;
 import net.fabricmc.toh.util.ModFlammableBlocks;
 import net.fabricmc.toh.util.ModStrippableBlocks;
-import net.fabricmc.toh.world.dimension.ModDimensions;
-import net.fabricmc.toh.world.feature.ModConfiguredFeatures;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,11 +15,9 @@ public class toh implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		ModConfiguredFeatures.registerConfiguredFeatures();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModFlammableBlocks.registerFlammableBlocks();
 		ModStrippableBlocks.registerStrippableBlocks();
-		ModDimensions.register();
 	}
 }
